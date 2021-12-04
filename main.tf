5909a430eb455c09de4732157486c42725895f5d
Daniel Gibbs <me@danielgibbs.co.uk>
2021-12-04 23:18:46 +0000

    add rdns

--- main.tf
+++ main.tf
@@ -21,24 +21,36 @@ provider "cloudflare" {
 }
 
 resource "linode_instance" "linode-server" {
-  label = "${var.server_name}"
-  image = "${var.linode_image}"
+  count = length(var.server_name)
+  label = "${var.server_name[count.index]}"
+  image = "${var.linode_image[count.index]}"
   tags = "${var.linode_tags}"
   region = "${var.linode_region}"
   type = "${var.linode_type}"
   authorized_keys = [var.authorized_keys]
+  watchdog_enabled = "true"
   #root_pass = var.root_pass
 }
 
 # Create a record
 resource "cloudflare_record" "cloudflare-dns" {
+  count = length(linode_instance.linode-server)
   zone_id = "${var.cloudflare_zone_id}"
-  name    = "${var.server_name}"
-  value   = "${linode_instance.linode-server.ip_address}"
+  name    = "${var.server_name[count.index]}"
+  value   = "${linode_instance.linode-server[count.index].ip_address}"
   type    = "A"
   proxied = false
 }
+resource "linode_rdns" "my_rdns" {
+  count = length(linode_instance.linode-server)
+  address = "${linode_instance.linode-server[count.index].ip_address}"
+  rdns = "${cloudflare_record.cloudflare-dns[count.index].hostname}"
+}
 
-output "instance_ip_addr" {
-  value = linode_instance.linode-server.ip_address
+output "linode_ip_addr" {
+  value = linode_instance.linode-server[*].ip_address
+}
+
+output "cloudflare_hostname" {
+  value = cloudflare_record.cloudflare-dns[*].hostname
 }
