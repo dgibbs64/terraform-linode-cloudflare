dcc3116034194485ffc1a17c3edaf67c6b68b7c6
Daniel Gibbs <me@danielgibbs.co.uk>
2025-08-04 20:29:08 +0000

    chore: downgrade `cloudflare` provider version to `~> 5.7.1`
    * Adjusted the version of the `cloudflare` provider to ensure compatibility.
    * This change may resolve issues related to the newer version.

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.7.2"
+      version = "~> 5.7.1"
     }
     time = {
       source  = "hashicorp/time"
